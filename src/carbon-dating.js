a224bf236c0f3ca49fa3247ccd96d6b62bc31247
Mikhail Kaladzianets <68966313+Mikhail1990-BY@users.noreply.github.com>
2020-10-26 15:55:34 +0300

    feat: add solution dream-team

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,14 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(num) {
+  if(typeof(num) !== 'string'){
+    return false;
+  } else if(typeof(parseInt(num)) === 'NaN'){
+    return false;
+  }
+  else if(num < '1' && num > '15'){
+    return false;
+  }
+  
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(Array.isArray(members)){
+    let str = [];
+      for (let member of members){
+        if (typeof member === 'string'){
+        let arrItem = member.toUpperCase().trim();
+        str.push(arrItem[0]);
+        }
+  } return str.sort().join('');
+  } else {
+    return false;
+  }
 };
