f5bfc11b76d0fca421b354cb29d723cf42a79940
Mikhail Kaladzianets <68966313+Mikhail1990-BY@users.noreply.github.com>
2021-02-03 17:20:57 +0300

    feat: add solutions

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,14 +3,13 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(num) {
-  if(typeof(num) !== 'string'){
-    return false;
-  } else if(typeof(parseInt(num)) === 'NaN'){
-    return false;
-  }
-  else if(num < '1' && num > '15'){
-    return false;
-  }
-  
+module.exports = function dateSample(sampleActivity = 'no-value') {
+  if (sampleActivity === 'no-value') return false;
+  if (typeof sampleActivity !== 'string') return false;
+  const sampleActivityNumeric = parseFloat(sampleActivity);
+  if(isNaN(sampleActivityNumeric)) return false;
+  if(sampleActivityNumeric > MODERN_ACTIVITY) return false;
+  if(sampleActivityNumeric === 0 || sampleActivityNumeric < 0) return false;
+  const k = Math.LN2 / HALF_LIFE_PERIOD;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivityNumeric) / k);
 };
